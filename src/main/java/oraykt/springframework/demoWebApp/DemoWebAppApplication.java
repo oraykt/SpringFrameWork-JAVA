b9d1abe4f8f1d5417466ef58ca21b82e78bdb27e
oraykt <oraykt@gmail.com>
2019-03-16 18:53:58 +0100

    Initial Commit

---
+++ src/main/java/oraykt/springframework/demoWebApp/DemoWebAppApplication.java
@@ -0,0 +1,13 @@
+package oraykt.springframework.demoWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oraykt/springframework/demoWebApp/DemoWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package oraykt.springframework.demoWebApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
